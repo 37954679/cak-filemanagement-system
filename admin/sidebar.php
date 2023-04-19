55bd3732c3078e2fd3838b90063bfd92c193560d
37954679 <beatriceodera75@gmail.com>
2023-04-19 08:41:08 +0300

    first commit

---
+++ admin/sidebar.php
@@ -0,0 +1,14 @@
+<div id="sidebar">
+	<ul id = "menu" class = "nav menu">
+		<li><a href = "home.php"><i class = "glyphicon glyphicon-home"></i> Dashboard</a></li>
+		<li><a href = ""><i class = "glyphicon glyphicon-user"></i> Accounts</a>
+			<ul>
+				<li><a href = "usermanagement.php"><i class = "glyphicon glyphicon-user"></i> Usermanagement</a></li>
+				<li><a href = "logs.php"><i class = "glyphicon glyphicon-user"></i> logs</a></li>
+				<li><a href = "requests.php"><i class = "glyphicon glyphicon-user"></i> requests</a></li>
+				<li><a href = "reports.php"><i class = "glyphicon glyphicon-user"></i> reports</a></li>
+				<li><a href="users.php"><i class = "glyphicon glyphicon-user"></i> users</a></li>
+			</ul>
+		</li>
+	</ul>
+</div>
